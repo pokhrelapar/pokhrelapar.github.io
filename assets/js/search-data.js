f9e99e1c36fff9e9c8968339564319e777d0ef4f
pokhrelapar <37117865+pokhrelapar@users.noreply.github.com>
2025-07-13 22:31:22 +0000

    Deploying to gh-pages from @ pokhrelapar/pokhrelapar.github.io@6e3f7891bd636fc3f4bee9cf6cb27e900b9a7af2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,9 +30,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-",
-          title: "",
-          description: "Courses I have taught over the years.",
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
