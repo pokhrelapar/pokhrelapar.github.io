38cb6beae560d42362643e621e29c936269877a1
pokhrelapar <37117865+pokhrelapar@users.noreply.github.com>
2025-07-14 05:48:42 +0000

    Deploying to gh-pages from @ pokhrelapar/pokhrelapar.github.io@7b6e65f8300498aab3dd5e5b12f3e0edaf1133f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -475,6 +475,51 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects1-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/1_project/";
+            },},{id: "projects1-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/2_project/";
+            },},{id: "projects1-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/3_project/";
+            },},{id: "projects1-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/4_project/";
+            },},{id: "projects1-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/5_project/";
+            },},{id: "projects1-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/6_project/";
+            },},{id: "projects1-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/7_project/";
+            },},{id: "projects1-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/8_project/";
+            },},{id: "projects1-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects1",handler: () => {
+              window.location.href = "/projects1/9_project/";
             },},{
         id: 'social-orcid',
         title: 'ORCID',
