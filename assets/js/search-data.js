bb2ca482a6db0bfe0558bab6c1239499c0fa7e82
pokhrelapar <37117865+pokhrelapar@users.noreply.github.com>
2025-07-14 04:37:03 +0000

    Deploying to gh-pages from @ pokhrelapar/pokhrelapar.github.io@5ff7db316ce80acde2485482087c8db335c10001 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,19 +30,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Courses and talks over the year",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
